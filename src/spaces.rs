02e750ab767a7da163297bb79aacc70da77f2024
daniel-redder <daniel-redder@users.noreply.github.com>
2024-12-10 12:14:09 -0600

    [skip ci] Autoformatting with rustfmt

--- src/spaces.rs
+++ src/spaces.rs
@@ -63,10 +63,12 @@ impl Space {
     // get the length of the space
     pub fn len(&self) -> usize {
         match self {
-            Space::Discrete {n: _, start: _} => panic!("Cannot call len on discrete space, see space.n"),
-            Space::Box {low: _, high: _} => panic!("Cannot call len on box space, see space.low, space.high, and space.shape"),
-            Space::Tuple {spaces} | Space::OneOf {spaces} | Space::Vector {spaces} => spaces.len(),
-            Space::Dict {spaces} => spaces.len(),
+            Space::Discrete { n: _, start: _ } => panic!("Cannot call len on discrete space, see space.n"),
+            Space::Box { low: _, high: _ } => {
+                panic!("Cannot call len on box space, see space.low, space.high, and space.shape")
+            }
+            Space::Tuple { spaces } | Space::OneOf { spaces } | Space::Vector { spaces } => spaces.len(),
+            Space::Dict { spaces } => spaces.len(),
         }
     }
 
